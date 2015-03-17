54c7faa3ee2ec67869eb6d91d092a4244cdd5511
ebrains <hzhu3@stevens.edu>
2015-03-17 14:13:50 -0400

    test002
    002

--- test/index.php
+++ test/index.php
@@ -1,11 +1,18 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html>
 <body>
 
 
 <form Name ="form1" Method ="POST" ACTION = "test.php">
-<INPUT TYPE = "Submit" Name = "Most popular" VALUE = "Most popular">
-<INPUT TYPE = "Submit" Name = "Selected" VALUE = "Selected">
+<input type = "Submit" name = "Most popular" value = "Most popular"><br />
+
+
+<input type="checkbox" name="destination[ ]" value = "1">empirestate</input><br />
+<input type="checkbox" name="destination[ ]" value = "2">nationalpark</input><br />
+<input type="checkbox" name="destination[ ]" value = "3">miamibeach</input><br />
+<input type="submit" value="Selected"><br />
+
+
 <input name="return1" type="text">
 <input name="return2" type="text">
 </form>
