c79488b9f1aadcec5c8bb9f9967c6468a600df54
ebrains <hzhu3@stevens.edu>
2015-03-17 13:49:50 -0400

    test001
    test001

---
+++ test/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<form Name ="form1" Method ="POST" ACTION = "test.php">
+<INPUT TYPE = "Submit" Name = "Most popular" VALUE = "Most popular">
+<INPUT TYPE = "Submit" Name = "Selected" VALUE = "Selected">
+<input name="return1" type="text">
+<input name="return2" type="text">
+</form>
+<?php
+echo "Test!";
+
+?>
+
+</body>
+</html>
+
