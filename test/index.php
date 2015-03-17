70f755e76f1f9695a75546a5b023697ea8cab05b
nkuclassical <yzhan14@stevens.edu>
2015-03-17 16:26:36 -0400

    revise value!

---
+++ test/index.php
@@ -0,0 +1,26 @@
+﻿<!DOCTYPE html>
+<html>
+<body>
+
+
+<form Name ="form1" Method ="POST" ACTION = "test.php">
+<input type = "Submit" name = "Most popular" value = "Most popular"><br />
+
+
+<input type="checkbox" name="destination[ ]" value = "1">empirestate</input><br />
+<input type="checkbox" name="destination[ ]" value = "2">nationalpark</input><br />
+<input type="checkbox" name="destination[ ]" value = "3">miamibeach</input><br />
+<input type="submit" name="Selected"><br />
+
+
+<input name="return1" type="text">
+<input name="return2" type="text">
+</form>
+<?php
+echo "Test!";
+
+?>
+
+</body>
+</html>
+
